f6663b2aca3fdd4b68cf934f3111955b43954b83
Anish Sapkota <anishsapkota52@gmail.com>
2024-07-22 11:54:16 +0545

    Update Felipe’s_Taqueria.py
    Its new version and updated

--- ProblemSet_3/Felipe’s_Taqueria.py
+++ ProblemSet_3/Felipe’s_Taqueria.py
@@ -5,39 +5,48 @@
 # // Assignment: Felipe's Taqueria      //
 # //                                    //
 # ////////////////////////////////////////
+def main():
+    is_available()
 
-# // Store the menu items and the cost as values
-MENU: dict[str, float] = {
-    "baja taco": 4.00,
-    "burrito": 7.50,
-    "bowl": 8.50,
-    "nachos": 11.00,
-    "quesadilla": 8.50,
-    "super burrito": 8.50,
-    "super quesadilla": 9.50,
-    "taco": 3.00,
-    "tortilla salad": 8.00
-}
+def is_availabe():
+    MENU= {
+        "baja taco": 4.00,
+        "burrito": 7.50,
+        "bowl": 8.50,
+        "nachos": 11.00,
+        "quesadilla": 8.50,
+        "super burrito": 8.50,
+        "super quesadilla": 9.50,
+        "taco": 3.00,
+        "tortilla salad": 8.00
+    }
+    
+    # // Store the total cost
+    total: int = 0
+    
+    while 1:
+        try:
+            # // Get the item
+            item= input("Item: ").lower()
+    
+            # // Increase the total by the value
+            # // from the item in the MENU map
+            if item in MENU:
+                total += MENU[item]
+    
+            # // Print the new total formatted to 2 decimal points
+                print(f"Total: ${total:.2f}")
+            elif item=='burger':
+                is_available()
+            else:
+                exit()
+                
+    
+        # // Except Control+D is pressed
+        except EOFError:
+            break
 
-# // Store the total cost
-total: int = 0
-
-while 1:
-    try:
-        # // Get the item
-        item: str = input("Item: ").lower()
-
-        # // Increase the total by the value
-        # // from the item in the MENU map
-        if item in MENU:
-            total += MENU[item]
-
-        # // Print the new total formatted to 2 decimal points
-        print(f"Total: ${total:.2f}")
-
-    # // Except Control+D is pressed
-    except EOFError:
-        break
+main()
 
 # https://github.com/settings/tokens
 # check50 cs50/problems/2022/python/taqueria
